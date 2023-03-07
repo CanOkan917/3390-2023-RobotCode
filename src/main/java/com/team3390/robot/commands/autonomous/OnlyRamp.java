556e744c55ab8d5d863ef0f032fd0d10b0831c22
Can Okan <tamadaminasordun@gmail.com>
2023-03-07 08:48:25 -0600

    Robot Practice Fixes

--- src/main/java/com/team3390/robot/commands/autonomous/OnlyRamp.java
+++ src/main/java/com/team3390/robot/commands/autonomous/OnlyRamp.java
@@ -1,10 +1,6 @@
 package com.team3390.robot.commands.autonomous;
 
-import com.team3390.robot.Constants;
-import com.team3390.robot.commands.drive.BalanceRobotCommand;
 import com.team3390.robot.commands.drive.DriveStraight;
-import com.team3390.robot.commands.drive.DriveUntilCustomNavXRoll;
-import com.team3390.robot.commands.drive.RotateToAngle;
 import com.team3390.robot.commands.utility.ResetSensorsCommand;
 import com.team3390.robot.subsystems.DriveSubsystem;
 
